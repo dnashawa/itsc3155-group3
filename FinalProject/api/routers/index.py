1be378f8f9a1513714a75f29e241920201814310
Abigail Lewis <125498411+Abigail-B-Lewis@users.noreply.github.com>
2023-12-02 17:07:58 -0500

    Added routers to index.py

--- FinalProject/api/routers/index.py
+++ FinalProject/api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, reviews, customers
+from . import orders, order_details, reviews, customers, promo_codes, sandwiches, sandwich_tags, resources, recipes
 
 
 def load_routes(app):
@@ -6,3 +6,8 @@ def load_routes(app):
     app.include_router(order_details.router)
     app.include_router(reviews.router)
     app.include_router(customers.router)
+    app.include_router(promo_codes.router)
+    app.include_router(sandwiches.router)
+    app.include_router(sandwich_tags.router)
+    app.include_router(recipes.router)
+    app.include_router(resources.router)
